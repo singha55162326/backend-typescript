eaf06c7b4852909d2d9be2faaa236395e981c451
singha55162326 <63039299+singha55162326@users.noreply.github.com>
2025-08-29 03:01:22 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -111,10 +111,12 @@ app.use(/(.*)/, (_req: Request, res: Response) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT: number = parseInt(process.env.PORT || "5000", 10);
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 export default app;
