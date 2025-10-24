ba3c8080c26c556e2c083edc76ef5d47ece670c5
singha55162326 <timvongphom@gmail.com>
2025-10-24 11:13:21 +0700

    updates

--- src/test-server.ts
+++ src/test-server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
